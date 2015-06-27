3144e915907ad3ef7a28034f08bcfd908cf5799f
Artem Devlysh <artem@devlysh.com>
2015-06-27 17:05:15 +0300

    Work in progress

--- src/index.js
+++ src/index.js
@@ -9,29 +9,28 @@
 
   var GOL = function () {
 
-    var Cell = function () {
-      var _alive = DEFAULT_ALIVE;
-      Object.defineProperty(this, 'alive', {
-        get: function() { return _alive; },
-        set: function (value) { _alive = Boolean(value); },
-      });
+    var Cell = function (x, y, element) {
+      this.alive = DEFAULT_ALIVE;
+      this.x = x;
+      this.y = y;
+      this.element = element;
     };
-
     Cell.prototype.toggleAlive = function (event) {
-      if (this.cellData.alive) {
-        this.cellData.alive = false;
-        this.classList.toggle('alive');
+      var element = event ? this : this.element;
+      if (this.alive) {
+        this.alive = false;
+        element.classList.toggle('alive');
       } else {
-        this.cellData.alive = true;
-        this.classList.toggle('alive');
+        this.alive = true;
+        element.classList.toggle('alive');
       }
     };
 
     /* UNIVERSE */
-    this.Universe = function (x, y) {
+    var Universe = function () {
       var thisUniverse = this;
-      this.width = x || DEFAULT_WIDTH;
-      this.height = y || DEFAULT_HEIGHT;
+      this.width = DEFAULT_WIDTH;
+      this.height = DEFAULT_HEIGHT;
 
       this.space = (function () {
         var s = new Array(thisUniverse.height);
@@ -41,31 +40,39 @@
         return s;
       })();
     };
-
-    this.Universe.prototype.create = function () {
+    Universe.prototype.create = function (width, height) {
+      width = width || this.width;
+      height = height || this.height;
       var table = d.createElement('table');
       table.classList.add('universe');
-      for (var y = 0; y < this.height; y++) {
+      for (var y = 0; y < height; y++) {
         var row = d.createElement('tr');
-        for (var x = 0; x < this.width; x++) {
-          var cell = d.createElement('td');
-          cell.cellData = new Cell();
-          cell.addEventListener('click', cell.cellData.toggleAlive);
-          this.space[y][x] = cell;
-          row.appendChild(cell);
+        for (var x = 0; x < width; x++) {
+          var cellElement = d.createElement('td');
+          cellElement.cell = new Cell(x, y, cellElement);
+          cellElement.addEventListener('click', cellElement.cell.toggleAlive);
+          this.space[y][x] = cellElement.cell;
+          row.appendChild(cellElement);
         }
         table.appendChild(row);
       }
       b.appendChild(table);
     };
+    this.universe = new Universe();
   };
 
   GOL.prototype.start = function () {};
   GOL.prototype.stop = function () {};
-  GOL.prototype.step = function () {};
+  GOL.prototype.step = function () {
+    var space = this.universe.space;
+    for (var x = 0; x < this.universe.width; x++) {
+      for (var y = 0; y < this.universe.height; y++) {
+        console.log(space[x][y]);
+      }
+    }
+  };
 
   var gameOfLife = new GOL();
-  var universe = new gameOfLife.Universe();
-  universe.create();
+  gameOfLife.universe.create();
 
 })(window);
